3cc45002e43d1657ab1f6573e90c1f91e80e510b
kstrinadka <kstrinadka@igatec.com>
2026-05-23 17:30:43 +0700

    init project

---
+++ src/test/java/com/kstrinadka/securebankapi/AbstractIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.kstrinadka.securebankapi;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class AbstractIntegrationTest {
+
+    private static final PostgreSQLContainer<?> POSTGRES =
+            new PostgreSQLContainer<>("postgres:15")
+                    .withDatabaseName("secure_bank_test")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    static {
+        POSTGRES.start();
+    }
+
+    @DynamicPropertySource
+    static void registerPostgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+        registry.add("spring.datasource.driver-class-name", POSTGRES::getDriverClassName);
+    }
+}
---
+++ src/test/java/com/kstrinadka/securebankapi/SecureBankApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kstrinadka.securebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class SecureBankApiApplicationTests extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
